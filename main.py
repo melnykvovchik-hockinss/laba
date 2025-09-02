022794abe9dcf9d45c000fff56cb9d311401f8f6
melnykvovchik-hockinss <melnyk.vovchik@gmail.com>
2025-09-02 21:02:21 +0300

    laba1

---
+++ main.py
@@ -0,0 +1,32 @@
+age = 2
+tasks_completed = 9
+coordinates = (1, 2)
+status = 'Work'
+quest_name = 'quest'
+range_values = (10, 15)
+score = 12
+time_interval = (1, 15)
+goal = 'Learn'
+level = 20
+
+print(age)
+print(tasks_completed)
+print(coordinates)
+print(status)
+print(quest_name)
+print(range_values)
+print(score)
+print(time_interval)
+print(goal)
+print(level)
+
+print(type(age))
+print(type(tasks_completed))
+print(type(coordinates))
+print(type(status))
+print(type(quest_name))
+print(type(range_values))
+print(type(score))
+print(type(time_interval))
+print(type(goal))
+print(type(level))
